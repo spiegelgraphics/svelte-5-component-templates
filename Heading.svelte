f9125fc82400f554bda107cac21eccb981c57775
Matthias Stahl <matthias.stahl87@web.de>
2024-11-13 12:34:38 +0100

    add Heading.svelte

---
+++ Heading.svelte
@@ -0,0 +1,34 @@
+<script>
+	const { title = '', subtitle = '' } = $props();
+</script>
+
+<div class="header">
+	{#if title}
+		<div class="title">{title}</div>
+	{/if}
+	{#if subtitle}
+		<div class="subtitle">{subtitle}</div>
+	{/if}
+</div>
+
+<style lang="scss">
+	@use './../scss/config';
+
+	.header {
+		line-height: 1.1;
+		margin-bottom: 0.75rem;
+	}
+
+	.title {
+		@include config.fontFamily(config.$spSlab);
+		font-size: 1.5rem;
+		padding-bottom: 0.0625rem;
+		@media screen and (max-width: 488px) {
+			font-size: 1.25rem;
+		}
+	}
+
+	.subtitle {
+		font-size: 1rem;
+	}
+</style>
