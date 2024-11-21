41fb869f98e6a426763dbe0a6a3023e4a906a227
Chris Kurt <135142712+chris-kurt-3@users.noreply.github.com>
2024-11-21 13:24:51 +0100

    Update Grid.svelte
    CSS Variable Doku

--- Grid.svelte
+++ Grid.svelte
@@ -7,6 +7,10 @@
      * The only restriction is the minimum column width.
      * 
      * @prop {HTMLElement} children - The child elements to be rendered inside the grid.
+     *
+     * @css {string} --column-min-width - CSS variable for the minimum column width. Default is 300px.
+     * @css {string} --row-gap - CSS variable for the row gap. Default is 1.5rem.
+     * @css {string} --column-gap - CSS variable for the column gap. Default is 1.5rem.
      * 
      * @example
      * <Grid>
