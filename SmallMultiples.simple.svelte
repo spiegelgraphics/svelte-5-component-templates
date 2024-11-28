1eb215f0569334d0cfdcece06b324f458fe065b1
Chris Kurt <135142712+chris-kurt-3@users.noreply.github.com>
2024-11-28 10:23:09 +0100

    Create SmallMultiples.simple.svelte

---
+++ SmallMultiples.simple.svelte
@@ -0,0 +1,49 @@
+<script>
+
+    import Grid from "$components/Grid.svelte";
+    import Button from "$components/Button.svelte";
+    import Datawrapper from "$components/Datawrapper.svelte";
+
+    const columnMinWidth = 303;
+
+    const fewItems = 4;
+    let visibleItems = $state(fewItems);
+    const buttonText = $derived(visibleItems === fewItems ? "Show More" : "Show Less");
+    const dataWrapperIds = ["cWEvG", "cWEvG", "cWEvG", "cWEvG", "cWEvG", "cWEvG", "cWEvG", "cWEvG", "cWEvG", "cWEvG", "cWEvG"];
+
+</script>
+
+<div class="content">
+
+    <div class="container">
+        <Grid --column-min-width="{columnMinWidth}px">
+            {#each dataWrapperIds as id, i}
+                {#if i < visibleItems }
+                    <Datawrapper id={id}/>
+                {/if}
+            {/each}
+        </Grid>
+    </div>
+
+    <div class="btn">
+        <Button buttonClicked={()=>{
+            visibleItems = visibleItems === fewItems ? dataWrapperIds.length || fewItems : fewItems;
+            }} secondary={true}>{buttonText}</Button>
+    </div>
+
+</div>
+
+<style lang="scss">
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .btn {
+    margin: 20px auto 0;
+  }
+
+</style>
+
