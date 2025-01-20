b901ac77617b4227fbb1440c972ab6e3be4a23a9
WahlsRina <138503890+WahlsRina@users.noreply.github.com>
2025-01-20 14:34:02 +0100

    slider: fix slider

--- Slider.svelte
+++ Slider.svelte
@@ -13,154 +13,138 @@
      * @event change - Fired when the slider value changes. The event detail contains the new value.
      */
 
-    let {
-        value,
-        min,
-        max,
-        step,
-        unit
-    } = $props();
+    import { getValue } from '$utils/value.js';
 
-    let barWidth = $derived(value / max * 100);
-    let click = $state(false);
-
-    const ScalePoint = (e) => {
-        click = true;
-    }
-
-    const ScalePointDown = (e) => {
-        click = false;
-    }
+    let { headline, value = $bindable({}), min, max, step, unit } = $props();
+    let barWidth = $derived((value - min) / (max - min) * 100);
 
 </script>
 
-<div class="outer-container" style:--barWidth={barWidth + "%"}>
-    <div class="thumb-container">
-        <div class="inner">
-            <div class="thumb"
-                style:transform={click ? "scale(1.5)" : "scale(1)"}
-            ></div>
+<div class="element-container">
+    {#if headline}
+        <div class="headline">{@html headline}</div>
+    {/if}
+
+    <div class="outer-container" style:--barWidth={barWidth + "%"}>
+        <div class="inner-container"></div>
+        <div class="progress-container">
+            <div class="thumb-left"></div>
+            <div class="bar" style="width: {barWidth}%"></div>
+            <div class="thumb" style="left: {barWidth}%"></div>
+            <div class="thumb-outer" style="left: {barWidth}%"></div>
+            <div class="value"
+                style:left={barWidth <= 50 ? "calc(var(--barWidth) + 25px)" : ""}
+                style:right={barWidth > 50 ? "calc(100% - var(--barWidth) + 20px)" : ""}
+                style:color={barWidth <= 50 ? "var(--int-font-color-link)" : "white"}>
+                {getValue(value, 0, '', false).str}&thinsp;{unit}
+            </div>
         </div>
-
-        <div class="value"
-            style:left={ barWidth <= 50 ? "calc(var(--barWidth) + 40px)" : ""}
-            style:right={ barWidth > 50 ? "calc(100% - var(--barWidth) + 40px)" : ""}
-            style:color={ barWidth < 50 ? "var(--int-font-color-link)" : "white"}
-        >
-            {value}&thinsp;{unit}
-        </div>
-    </div>
-
-    <div 
-        class="bar" 
-        style:width={barWidth}% 
-        style:border-radius={barWidth > 50 ? "0 18px 18px 0" : "0" }
-        >
-    </div>
-
-    <div class="input-container">
-        <input 
-            type="range"
-            id="slider"
-            min={min}
-            max={max}
-            step={step}
-            bind:value
-            onmousedown={ScalePoint}
-            onmouseup={ScalePointDown}
-            ontouchstart={ScalePoint}
-            ontouchend={ScalePointDown}
-        />
+        <input type="range" bind:value min={min} max={max} step={step} />
     </div>
 </div>
 
+    
 <style lang="scss">
-	@use './../../scss/config';
-
-    input {
-        width: 100%;
-    }
-
-    .input-container {
+    @use './../../scss/config';
+    
+    .element-container {
         display: flex;
-        height: 36px;
-        position: relative;
-        z-index: 2; 
+        flex-direction: column;
+        user-select: none;
+        // overflow: hidden;
     }
-
+    
     .outer-container {
-        border-radius: 18px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 36px;
+        width: 100%;
         position: relative;
-        overflow: hidden; 
+        background-color: var(--int-button-background-color);
+        border-radius: 18px;
+        overflow: hidden;
     }
-
-    .thumb-container{
+    
+    .inner-container, 
+    .progress-container {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        width: calc(100% - 36px);
+        background-color: transparent;
+    }
+    
+    .progress-container {
         display: flex;
         align-items: center;
-        justify-content: center;
-        position: absolute;
-        width: 100%;
-        background-color: var(--int-button-background-color);
-        height: 36px;
-        pointer-events: none;
-
-        .inner{
-            width: calc(100% - 36px);
-
-
-            .thumb{
-                width: 14px;
-                height: 14px;
-                background-color: white;
-                border-radius: 50%;
-                position: relative;
-                left: calc(var(--barWidth) - 7px);
-                z-index: 3;       
-            }
-        }
+        height: 16px;
     }
-
+    
     .bar {
-        position: absolute; 
-        background-color: var(--int-font-color-link);
         height: 36px;
-        top: 0;
-        left: 0;
-        z-index: 1; 
+        background-color: var(--int-font-color-link);
+        position: absolute;
+    }
+    
+    .thumb {
+        position: absolute;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: 14px;
+        height: 14px;
+        background-color: white;
+        border-radius: 50%;
+        z-index: 20;
     }
 
+    .thumb-outer {
+        position: absolute;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: 36px;
+        height: 36px;
+        background-color: var(--int-font-color-link);
+        border-radius: 50%;
+        z-index: 10;
+    }
+    
+    .thumb-left {
+        position: absolute;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: 36px;
+        height: 36px;
+        background-color: var(--int-font-color-link);
+        border-radius: 50%;
+        left: 0;
+        z-index: 10;
+    }
+    
     .value {
         position: absolute;
         color: var(--int-font-color-link);
         font-size: config.$font-size-l;
-        @include config.fontFamily(config.$spSansBold); 
+        @include config.fontFamily(config.$spSansBold);
         z-index: 4;
         white-space: nowrap;
     }
-
+    
     input[type="range"] {
+        position: absolute;
+        width: calc(100% - 36px);
+        height: 36px;
         appearance: none;
         background: transparent;
+        opacity: 0;
+        z-index: 30;
     }
-
-    input[type="range"]::-webkit-slider-thumb {
-        appearance: none;
-        width: 36px;
-        height: 36px;
-        background-color: var(--int-font-color-link);
-        border-radius: 0 50% 50% 0;
-        cursor: pointer;
-        position: relative;
+    
+    .headline {
+        font-size: config.$font-size-m;
+        color: var(--int-font-color-default);
+        margin-bottom: 0.5rem;
+        @include config.fontFamily(config.$spSansBold);
     }
-
-    input[type="range"]::-moz-range-thumb {
-        width: 36px;
-        height: 36px;
-        background-color: var(--int-font-color-link);
-        border-radius: 0 50% 50% 0;
-        border: none;
-        cursor: pointer;
-        position: relative;
-    }
-
-</style>
+    </style>
+    
