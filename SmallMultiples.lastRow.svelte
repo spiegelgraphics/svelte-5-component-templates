fddbd8362ddd099d1ec3b14dfe8ad4a517e4853e
Chris Kurt <135142712+chris-kurt-3@users.noreply.github.com>
2025-01-22 12:51:21 +0100

    Update SmallMultiples.lastRow.svelte
    -.container

--- SmallMultiples.lastRow.svelte
+++ SmallMultiples.lastRow.svelte
@@ -54,7 +54,7 @@
     <div class="debug">fewItems: {fewItems}<br>visibleItems: {visibleItems}<br>length: {dataWrapperIds.length}</div>
 
 
-    <div class="container" bind:clientWidth={gridWidth} bind:this={gridContainer}>
+    <div class="conti" bind:clientWidth={gridWidth} bind:this={gridContainer}>
         <Grid --column-min-width="{columnMinWidth}px">
             {#each dataWrapperIds as id, i}
                 {#if i < visibleItems }
@@ -105,6 +105,12 @@
     height: 200px;
     box-shadow: red 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
   }
+
+.conti{
+    display: block;
+    width: 100%;
+  }
+
 
 </style>
 
