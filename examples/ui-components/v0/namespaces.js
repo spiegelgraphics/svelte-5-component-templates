71c874e7e3cdb222708d87a017a2e54718202ca0
KalinowskiF <frank.kalinowski@spiegel.de>
2025-02-18 14:08:17 +0100

    remove examples; update gitignore

--- examples/ui-components/v0/namespaces.js
+++
@@ -1,3 +0,0 @@
-/** @namespace d.json_output */
-/** @namespace d._source */
-/** @namespace d.reference */
--- examples/ui-components/v0/src/lib/util/getCurrentScriptTag.js
+++
@@ -1,6 +0,0 @@
-export const getScriptTag = (importMeta = {}) => {
-    return document.currentScript || (() => {
-        const url = (importMeta.url || "").replace(/^.+?(\/src\/.+?.js)+?/, "$1");
-        return document.querySelector("script[src='" + url + "']");
-    })();
-};
